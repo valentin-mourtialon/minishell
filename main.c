05d28d7709bdea2ec76214fc85439a8e190ed67a
valentin-mourtialon <valentinmourtialon@protonmail.com>
2023-01-19 14:24:40 +0100

    valentin: add .gitignore and main

---
+++ main.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmourtia <vmourtia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/19 14:23:17 by vmourtia          #+#    #+#             */
+/*   Updated: 2023/01/19 14:23:25 by vmourtia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <lexer.h>
+
+int	main(void)
+{
+	char	*cmd;
+	char	*ptrstart;
+	t_lexer	*lexer;
+
+	signal(SIGINT, handle_sigquit);
+	signal(SIGQUIT, SIG_IGN);
+	while (1)
+	{
+		cmd = readline("lexer ");
+		if (!cmd)
+			break ;
+		ptrstart = cmd;
+		lexer = NULL;
+		while (*cmd)
+			get_next_token(&cmd, &lexer);
+		parser(&lexer);
+		free(ptrstart);
+		print_token(lexer);
+	}
+	return (0);
+}
